3feb295d41e7177cd1c961f8b421263ac5425ebc
RitamhD <119100542+RitamhD@users.noreply.github.com>
2024-03-24 16:46:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/* JavaScript */
+window.onload = function(){
+    var prevScrollpos = window.scrollY;
+    window.onscroll = function() {
+        var currentScrollPos = window.scrollY;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-50px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+}
